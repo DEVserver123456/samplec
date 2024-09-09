fb3c1f849e558e17002ed79ee149f1b41ff40f74
admin <madhubellamkonda76@gmail.com>
2024-09-09 11:57:41 +0530

    adding inputs

--- mathoperations.c
+++ mathoperations.c
@@ -9,6 +9,6 @@ printf("Enter the value of b \n");
 scanf("%d",&b);*/
 printf("\naddition of two numbers%d\n",a+b);
 printf("\nsubstraction of two numbers%d\n",a-b);
-printf("\nmultiplication of two numbers%d\n",a+b);
+printf("\nmultiplication of two numbers%d\n",a*b);
 
 }
