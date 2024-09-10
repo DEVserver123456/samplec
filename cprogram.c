342c2d4c2e451de99da0b7f4e840c86f98e49383
admin <madhubellamkonda76@gmail.com>
2024-09-10 17:20:53 +0530

    testing

--- cprogram.c
+++ cprogram.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main()
 {
-int a,b,sum,sub,mul;
-printf("Enter the value of a\n");
+int a=5,b=90,sum,sub,mul;
+/*printf("Enter the value of a\n");
 
 scanf("%d",&a);
 printf("Enter the value of b \n");
-scanf("%d",&b);
+scanf("%d",&b);*/
 printf("\naddition of two numbers%d\n",a+b);
 printf("\nsubstraction of two numbers%d\n",a-b);
-printf("\nmultiplication of two numbers%d\n",a+b);
+printf("\nmultiplication of two numbers%d\n",a*b);
 
 }
